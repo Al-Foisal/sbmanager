b6244e40a811b1e57169ecf44908c9ef9dd8f05c
Md Hafiz Al Foisal <alfoisla940700@gmail.com>
2022-06-12 13:11:51 +0600

    backend

---
+++ app/Models/Area.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Area extends Model {
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function division() {
+        return $this->belongsTo(Division::class);
+    }
+
+    public function district() {
+        return $this->belongsTo(District::class);
+    }
+}
--- resources/views/customer/layouts/partials/_control-sidebar.blade.php
+++ resources/views/customer/layouts/partials/_control-sidebar.blade.php
@@ -1,10 +1,20 @@
-<aside class="control-sidebar control-sidebar-dark">
+<aside class="control-sidebar control-sidebar-dark bg-success">
     <!-- Control sidebar content goes here -->
     <div class="p-3">
-        <h5>Title</h5>
-        <form action="{{ route('customer.logout') }}" method="post">
-            @csrf
-            <button type="submit" class="btn btn-link">Logoutd</button>
-        </form>
+        <h5 style="border-bottom: 1px solid;">Store Info</h5>
+        <ul style="list-style-type: disclosure-closed;">
+            <li style="background:white;" class="mb-1">
+                <form action="{{ route('customer.logout') }}" method="post">
+                    @csrf
+                    <button type="submit" class="btn btn-link">Logoutd</button>
+                </form>
+            </li>
+            <li style="background:white;">
+                <form action="{{ route('customer.logout') }}" method="post">
+                    @csrf
+                    <button type="submit" class="btn btn-link">Logoutd</button>
+                </form>
+            </li>
+        </ul>
     </div>
 </aside>
