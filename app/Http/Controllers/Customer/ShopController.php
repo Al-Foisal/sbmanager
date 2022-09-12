171d09a2797ea99f41a0c38aaf9ea7ecfdea9189
Md Hafiz Al Foisal <alfoisla940700@gmail.com>
2022-09-12 11:14:08 +0600

    shop api updated

--- app/Http/Controllers/Customer/ShopController.php
+++ app/Http/Controllers/Customer/ShopController.php
@@ -80,6 +80,10 @@ class ShopController extends Controller {
         Shop::create([
             'customer_id'        => Auth::id(),
             'name'               => $request->name,
+            'division_id'        => $request->division_id,
+            'district_id'        => $request->district_id,
+            'area_id'            => $request->area_id,
+            'shop_type_id'       => $request->shop_type_id,
             'image'              => $final_name1,
             'payment_link'       => Str::slug($request->name),
             'online_market_link' => Str::slug($request->name),
